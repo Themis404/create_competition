612b1e2d8b0931ad069c5548e3687cfdae33af1b
bigman212 <bigman212@yandex.ru>
2018-07-08 23:21:06 +1000

    refactor, fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import InputInfo from './components/InputInfo.js'
-import TabelComp from './components/TabelComp.js'
+import CompetitionTable from './components/CompetitionTable.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
      <div className='flex-container'>
-      <InputInfo />
-      <TabelComp />
+       <InputInfo />
+       <CompetitionTable />
      </div>
     );
   }
