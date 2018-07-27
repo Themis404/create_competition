ba39be741051028cc1a063f4a6fa6aa120a5db12
Cridea <cridea666@mail.ru>
2018-07-27 15:35:53 +1000

    строка поиска

--- src/containers/mainPageContainer.jsx
+++ src/containers/mainPageContainer.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Redirect } from 'react-router-dom';
 import CompetitionTable from '../components/CompetitionTable.js'
 import BaseComponent from './baseComponent'
+import lupa from './lup.png'
 
 class MainPage extends BaseComponent {
     constructor(params) {
@@ -17,6 +18,13 @@ class MainPage extends BaseComponent {
         }
         return (
           <div className='marginElem'>
+             <div className="Search">
+                <form className="searchF">
+                     <input type="search" name="q" placeholder="Поиск..."/> 
+                     <button className='SearchButton'><img src={lupa} alt='lupa' className="lupa"/></button>
+                      {/* <input type="submit" value="Найти"/> */}
+                 </form>
+              </div>
               <CompetitionTable />
           </div>
         );
