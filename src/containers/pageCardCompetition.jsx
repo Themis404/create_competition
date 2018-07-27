426167a1789a835cf0c20365cbfe4b2efd88b731
Cridea <cridea666@mail.ru>
2018-07-27 17:32:31 +1000

    добавлена таблица и кнопка

--- src/containers/pageCardCompetition.jsx
+++ src/containers/pageCardCompetition.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import CardCompetition from '../components/cardCompetition'
 import BaseComponent from '../containers/baseComponent'
 import { Redirect } from 'react-router-dom';
+import DaysTable from '../components/daysTable'; 
+import CreateDay from '../components/createDay'; 
 
 class PageCardCompetition extends BaseComponent {
     constructor(params) {
@@ -18,6 +20,8 @@ class PageCardCompetition extends BaseComponent {
         <div className='marginElem'>
           <button onClick={() => this.goToState('/main')} className='button flex-container'>Back</button>
           <CardCompetition id={this.props.match.params.id}/>
+          <button onClick={() => this.goToState('/create-day')} className='button flex-container'>Create day</button>
+          <DaysTable/>
         </div>
       );
     }
