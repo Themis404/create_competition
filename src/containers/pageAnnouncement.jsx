16e7873e4cb2a68bc65bce22cbab39f9af2664e0
Cridea <cridea666@mail.ru>
2018-07-31 10:53:46 +1000

    просто форму добавила

--- src/containers/pageAnnouncement.jsx
+++ src/containers/pageAnnouncement.jsx
@@ -17,6 +17,7 @@ class PageAnnouncement extends BaseComponent {
         <div className='marginElem'>
           <button onClick={() => this.goToState('/main')} className='button flex-container'>Back</button>
           <p>IMPORTANT MESSAGE</p>
+          <textarea rows="20" className='ImportantMessage'/>
         </div>
       );
     }
