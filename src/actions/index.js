d7dfe5e4427e100eae5177739952e392459aab15
Cridea <cridea666@mail.ru>
2018-08-01 14:44:15 +1000

    перенесен апи

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export function fetchSearch(name) {
-    return axios.get('`https://afternoon-woodland-86438.herokuapp.com/competitions/list?page=0&size=5&sort=name,asc&searchByName',{params:{
-        q:name
+    return axios.get('https://afternoon-woodland-86438.herokuapp.com/competitions/list?page=0&size=5&sort=name,asc',{params:{
+      searchByName: name
     }}).then(res => res.data);
 }
 
