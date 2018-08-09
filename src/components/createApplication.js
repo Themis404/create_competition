e5dce53be4e657ba80db8bae48a74df2f048368d
Themis404 <legen_waitforit_dary@mail.ru>
2018-08-09 11:15:54 +1000

    *

--- src/components/createApplication.js
+++ src/components/createApplication.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Redirect} from 'react-router-dom';
 import BaseComponent from '../containers/baseComponent'
 import ReactDOM from 'react-dom';
+import * as actions from '../actions/index';
 
 class createApplication extends BaseComponent {
 
@@ -18,13 +19,31 @@ class createApplication extends BaseComponent {
       recingMastery: '',
       vehicleType: '',
       gender: '',
-      competition: ''
+      competition: '',
+      competitionId: ''
     }
+  }
+
+  componentDidMount() {
+    this.getCompetitionInfo();
+  }
+
+  getCompetitionInfo = () => {
+    actions.list({
+    }).then((content) => {
+          this.setState({
+            content: content
+          });
+        });
+  }
+
+  getActivCompetition = () => {
+
   }
 
   handleSubmit = (e) => {
     console.log(this.state);
-    fetch('https://afternoon-woodland-86438.herokuapp.com/competitions/create', {
+    fetch('https://afternoon-woodland-86438.herokuapp.com/participants/create', {
       method: 'POST',
       headers: {
         'Access-Control-Allow-Headers': 'origin, content-type, accept',
@@ -42,11 +61,12 @@ class createApplication extends BaseComponent {
         email: this.state.email,
         recingMastery: this.state. recingMastery,
         vehicleType: this.state.vehicleType,
-        competition: this.state.competition
+        competition: this.state.competition,
+        gender: this.state.gender
       })
     }).then(res => {
       console.log(res);
-      this.setState({name: '', surname: '', fatherName: '', age: '', phone: '', emergencyPhone: '', email: '', recingMastery: '', vehicleType: ''});
+      this.setState({name: '', surname: '', fatherName: '', age: '', phone: '', emergencyPhone: '', email: '', recingMastery: '', vehicleType: '', gender: ''});
       console.log(this.state);
       res.ok ? console.log('success') : console.warn('something gone wrong');
     });
@@ -76,14 +96,21 @@ class createApplication extends BaseComponent {
   updateEmergencyPhone(e) {
     this.setState( {emergencyPhone: e.target.value} )
   }
+
   updateEmail(e) {
     this.setState( {email: e.target.value} )
   }
+
   updateRecingMastery(e) {
     this.setState( {recingMastery: e.target.value} )
   }
+
   updateVehicleType(e) {
     this.setState( {vehicleType: e.target.value} )
+  }
+
+  updateGender(e) {
+    this.setState( {gender: e.target.value} )
   }
 
   render() {
@@ -105,32 +132,36 @@ class createApplication extends BaseComponent {
                   onChange={e => this.updateSurname(e)}></input>
           <h5><p className='col-md-12 nonePadding marginTopStandart'>Отчество</p></h5>
           <input id='inputLastName' className="form-control" placeholder = "Иванович"
-                  value={this.state.lastName}
-                  onChange={e => this.updateLastName(e)}></input>
+                  value={this.state.fatherName}
+                  onChange={e => this.updateFatherName(e)}></input>
           <h5><p className='col-md-12 nonePadding marginTopStandart'>Возраст*</p></h5>
           <input required type = 'number' id='inputAge' className="form-control"
                   value={this.state.age}
                   onChange={e => this.updateAge(e)}></input>
+                <h5><p className='col-md-12 nonePadding marginTopStandart'>Пол*</p></h5>
+          <input required id='inputSurname' className="form-control" placeholder = "MALE/FEMALE"
+                  value={this.state.gender}
+                  onChange={e => this.updateGender(e)}></input>
           <h5><p className='col-md-12 nonePadding marginTopStandart'>Введите свой номер телефона*</p></h5>
           <input required type='tel' id='inputNumber' className="form-control" placeholder = "+7 "
-                  value={this.state.number}
-                  onChange={e => this.updateNumber(e)}></input>
+                  value={this.state.phone}
+                  onChange={e => this.updatePhone(e)}></input>
           <h5><p className='col-md-12 nonePadding marginTopStandart'>Введите номер телефона в случае ЧС*</p></h5>
           <input required type='tel' id='inputNumberEs' className="form-control" placeholder = "+7 "
-                  value={this.state.numberEs}
-                  onChange={e => this.updateNumberEs(e)}></input>
+                  value={this.state.emergencyPhone}
+                  onChange={e => this.updateEmergencyPhone(e)}></input>
           <h5><p className='col-md-12 nonePadding marginTopStandart'>Адрес электронной почты*</p></h5>
           <input required type='email' id='inputEmail' className="form-control" placeholder = "email@email.ru"
                   value={this.state.email}
                   onChange={e => this.updateEmail(e)}></input>
           <h5><p className='col-md-12 nonePadding marginTopStandart'>Уровень подготовки*</p></h5>
-          <input required id='inputLevel' className="form-control"
-                  value={this.state.level}
-                  onChange={e => this.updateLevel(e)}></input>
+          <input required id='inputLevel' className="form-control"  placeholder = 'NEWBIE, INTERMEDIATE, PROFESSIONAL'
+                  value={this.state.recingMastery}
+                  onChange={e => this.updateRecingMastery(e)}></input>
           <h5><p className='col-md-12 nonePadding marginTopStandart'>Тип транспортного средсва*</p></h5>
-          <input required id='inputTypeTransport' className="form-control"
-                  value={this.state.typeTransport}
-                  onChange={e => this.updateTypeTransport(e)}></input>
+          <input required id='inputTypeTransport' className="form-control" placeholder = 'MOTORCYCLE/ATV'
+                  value={this.state.vehicleType}
+                  onChange={e => this.updateVehicleType(e)}></input>
           <button type="submit" className="btn btn-success col-md-4 col-md-offset-4 marginTopStandart marginBotStandart">Отправить</button>
         </form>
       </div>
---
+++ src/components/pointCreate.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import {Redirect} from 'react-router-dom';
+import BaseComponent from '../containers/baseComponent'
+import ReactDOM from 'react-dom';
+
+class CreateDayForm extends BaseComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      sequenceNumber: '',
+      name: '',
+      competitionDayId: '',
+      placePointType: ''
+  }
+
+  componentDidMount() {
+    this.setState({
+      competitionId: this.props.id
+    });
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({
+      competitionId: newProps.id
+    });
+  }
+
+  handleSubmit = (e) => {
+    console.log(this.state);
+    fetch('https://afternoon-woodland-86438.herokuapp.com/days/create', {
+      method: 'POST',
+      headers: {
+        'Access-Control-Allow-Headers': 'origin, content-type, accept',
+        'Access-Control-Allow-Origin': '*',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        competitionId: this.state.competitionId,
+        name: this.state.name,
+        date: this.state.date,
+        timeStart: this.state.timeStart,
+        timeFinish: this.state.timeFinish,
+        sequenceNumber: this.state.sequenceNumber
+      })
+    }).then(res => {
+      console.log(res);
+      this.setState({name: '', date: '', timeStart: '', timeFinish: '', sequenceNumber: '', competitionId: ''});
+      console.log(this.state);
+      this.checkFieldsEmpty();
+      res.ok ? console.log('success') : console.warn('something gone wrong');
+    });
+    e.preventDefault();
+  };
+
+  updateName(e) {
+    this.setState( {name: e.target.value} );
+  }
+
+  updateTimeStart(e) {
+    this.setState( {timeStart: e.target.value} )
+  }
+
+  updateTimeFinish(e) {
+    this.setState( {timeFinish: e.target.value} )
+  }
+
+  updateDate(e) {
+    this.setState( {date: e.target.value} )
+  }
+
+  updateSequenceNumber(e) {
+    this.setState( {sequenceNumber: e.target.value} )
+  }
+
+  updateCompetitionId() {
+    this.setState( {competitionId: this.props.id} )
+    console.log(this.state.competitionId)
+  }
+
+  render() {
+    if (this.reload) {
+      this.reload = false;
+      return <Redirect to={this.redirect} push={true}/>;
+    }
+    return (
+        <div className="col-md-12">
+        <button onClick={() => this.goToState('/competition/' + this.props.id)} className='btn btn-warning col-md-2 noneFloat'>Back</button>
+          <form onSubmit={this.handleSubmit} className=''>
+            <h2 className="text-center col-md-12 marginTopStandart">Create day</h2>
+            <h5><p className='col-md-12 nonePadding marginTopStandart'>Name</p></h5>
+            <input required name='name' className="form-control" placeholder = "NAME DAY"
+                    value={this.state.name}
+                    onChange={ e => this.updateName(e)}></input>
+            <h5><p className='col-md-12 nonePadding marginTopStandart'>Date</p></h5>
+            <input required type='date' name='date'className="form-control" placeholder = "DD-MM-YYYY"
+                    value={this.state.date}
+                    onChange={ e => this.updateDate(e)}></input>
+            <h5><p className='col-md-12 nonePadding marginTopStandart'>Time Start</p></h5>
+            <input required type='time' name='timeStart' className="form-control" placeholder = "HH-MM"
+                    value={this.state.timeStart}
+                    onChange={ e => this.updateTimeStart(e)}></input>
+            <h5><p className='col-md-12 nonePadding marginTopStandart'>Time Finish</p></h5>
+            <input required type='time' name='timeFinish' className="form-control" placeholder = "HH-MM"
+                    value={this.state.timeFinish}
+                    onChange={ e => this.updateTimeFinish(e)}></input>
+                  <h5><p className='col-md-12 nonePadding marginTopStandart'>Sequence Number</p></h5>
+            <input required name='numberDay' className="form-control" placeholder = "Number day"
+                    value={this.state.sequenceNumber}
+                    onChange={ e => this.updateSequenceNumber(e)}></input>
+            <button type="submit" name='submit' className="btn btn-success col-md-3 col-md-offset-4 marginTopStandart marginBotStandart" >Create</button>
+          </form>
+        </div>
+    )
+  }
+}
+
+export default CreateDayForm;
