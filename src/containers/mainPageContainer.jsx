178bdf7ee4c73b35388bb6a0f9f5dff9b32e6d5b
Cridea <cridea666@mail.ru>
2018-08-22 22:36:56 +1000

    добавлены иконки

--- src/containers/mainPageContainer.jsx
+++ src/containers/mainPageContainer.jsx
@@ -4,6 +4,7 @@ import { Redirect } from 'react-router-dom';
 import BaseComponent from './baseComponent'
 import SearchBar from '../components/SearchBar.js';
 import * as actions from '../actions/competitions';
+import ModalDialog from '../components/ModalDialog';
 
 class MainPage extends BaseComponent {
     constructor(props) {
@@ -72,13 +73,33 @@ class MainPage extends BaseComponent {
           sortValue: this.state.sortValue === 'dateStart,asc' ? 'dateStart,desc' : 'dateStart,asc'
         }, () => this.getCompetitionInfo());
       }
-
+      
       render() {
         if (this.reload) {
             this.reload = false;
             return <Redirect to={this.redirect} push={true} />;
         }
         return (
+          <div>
+          <div class="modal" id="ModalDialog" tabindex="-1" role="dialog">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p>Modal body text goes here.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
           <div className='container'>
             <div className="center-block">
               <SearchBar onSearch={ e => this.setState({searchByName: e}, () => this.getCompetitionInfo())}/>
@@ -86,14 +107,15 @@ class MainPage extends BaseComponent {
                 <button onClick={() => this.goToState('/create-competition')} className='btn btn-info heightButton col-md-2'><span className='fas fa-plus'></span> Create competition</button>
 
               </div>
-              <h3><p className="text-center col-md-4 col-md-offset-4 nonePadding">COMPETITIONS TABLE</p></h3>
+              <h2><p className="text-center col-md-4 col-md-offset-4 nonePadding">COMPETITIONS TABLE</p></h2>
               <div className='row container col-md-center'>
-                <table className="table table-condensed table-striped table-hover">
+                <table className="table table-condensed table-striped table-hover ">
                   <thead>
                     <tr className="info active">
-                      <th className="text-center col-md-3" onClick={() => this.sortNameCompetitions()}>NAME COMPETITION <span class="fa fa-sort float-right"></span></th>
+                      <th className="text-center col-md-2" onClick={() => this.sortNameCompetitions()}>NAME COMPETITION <span class="fa fa-sort float-right"></span></th>
                       <th className="text-center col-md-2" onClick={() => this.sortDateCompetitions()}>DATE START COMPETITION <span class="fa fa-sort float-right"></span></th>
                       <th className="text-center  col-md-2">DATE END COMPETITION</th>
+                      <th className="text-center col-md-1"></th>
                       {/* <th className="th">Description</th> */}
                     </tr>
                   </thead>
@@ -101,9 +123,17 @@ class MainPage extends BaseComponent {
                     {
                       !!this.state.content.content && this.state.content.content.map((contentRow, key) =>
                           <tr key={key} className="tr">
-                            <td className="text-center col-md-3" onClick={() =>  this.goToState('/competition/'+contentRow.id)}>{contentRow.name}</td>
+                            <td className="text-center col-md-2" onClick={() =>  this.goToState('/competition/'+contentRow.id)}>{contentRow.name}</td>
                             <td className="text-center col-md-2">{contentRow.dateStart}</td>
                             <td className="text-center col-md-2">{contentRow.dateFinish}</td>
+                            <td className="text-center col-md-1">
+                              <button id="btn-tooltip" type="button" class="btn btn-default" aria-label="Eye" title="Activated">
+                                <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                              </button>
+                              <button data-target="#ModalDialog" data-toggle="modal" id="btn-tooltip" type="button" class="btn btn-default" aria-label="Remove" title="Delete">
+                                <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                              </button>
+                            </td>
                             {/* <td className="td">{contentRow.description}</td> */}
                           </tr>
                       )
@@ -145,6 +175,7 @@ class MainPage extends BaseComponent {
             </div>
 
             </div>
+          </div>
           </div>
         )
     }
